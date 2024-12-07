17ef2707d65b9bbcb188842fff84da262a12dbae
mcyasar <muratcany0167@gmail.com>
2024-12-07 15:28:55 +0300

    Main page in çalıştırılması

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 // import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { RegisterComponent } from './register/register.component';
-import { IndexComponent } from './index/index.component';  // Correct path
+import { IndexComponent } from './index/index.component';
 
 export const routes: Routes = [
 //   { path: 'home', component: HomeComponent },
   { path: 'girisyap', component: SigninComponent },
   { path: 'kaydol', component: RegisterComponent },
-  { path: 'index', component: IndexComponent },  // Correct routing
-  { path: '', redirectTo: '/index', pathMatch: 'full' }  // Default route
+  { path: 'index', component: IndexComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
 
+  isSearchedFormActive: boolean = false;
+  toggleSearchFrom (event : MouseEvent):void {
+    this.isSearchedFormActive = !this.isSearchedFormActive;
+    event.stopPropagation();
+  }
+  closedSearchForm(): void{
+    this.isSearchedFormActive =false; 
+  }
+  
 }
