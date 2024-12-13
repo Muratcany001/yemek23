854e5cb038b7db93c5fc1aa76f3bab67b666378f
mcyasar <muratcany0167@gmail.com>
2024-12-13 17:17:53 +0300

    register ve signin componentlerinin api'ye bağlanması

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+/**
+ * Component for user registration
+ */
 @Component({
   selector: 'app-register',
-  standalone:true,
+  standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  encapsulation: ViewEncapsulation.Emulated, // Varsayılan encapsulation
+  encapsulation: ViewEncapsulation.Emulated // Default encapsulation
 })
 export class RegisterComponent implements OnInit {
+  // User form fields
   userName: string = '';
   userEmail: string = '';
   userPassword: string = '';
   
   constructor() { }
+
+  // Lifecycle hook
   ngOnInit(): void {
     console.log("Register component loaded!");
   }
-  registerUser(): void {
+
+  // Handle user registration
+  onRegister(): void {
     if (this.userName && this.userEmail && this.userPassword) {
       console.log("Kullanıcı kaydedildi:", this.userName, this.userEmail);
     } else {
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
-export class SigninComponent {}
+export class SigninComponent {
+  mail:string = "";
+  password:string = "";
+  constructor(private apiService:ApiService){}
+
+  onLogin():void {
+    const credentials = {
+      mail:this.mail,
+      password:this.password
+    }
+    this.apiService.login(credentials).subscribe((res)=>{
+      console.log("Giriş başarılı",res);
+    },
+    (error)=>{
+      console.error("Giriş hatası",error);
+    }
+  )
+  }
+
+}
+
